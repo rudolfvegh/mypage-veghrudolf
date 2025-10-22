496915c0810623ae90fab0748bcc36aa230f5a1f
rudolfvegh <63446504+rudolfvegh@users.noreply.github.com>
2025-10-22 13:13:00 +0200

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,11 @@
+// CaruCore AI Chat snippet
+window.ChatConfig = {
+  api_key: '82878be5-d561-48d3-9c3c-8fc967755e61',
+};
+
+(function() {
+  const s = document.createElement('script');
+  s.src = 'https://carucore-frontend-dot-carussel-dwt.ew.r.appspot.com/chat-loader.js';
+  s.async = true;
+  document.body.appendChild(s);
+})();
